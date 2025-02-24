ade1537124b1e3e206dfcdf52a9ac0f339b5f2ca
Padmashree1384 <padmashree1384@gmail.com>
2025-02-24 15:32:12 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,13 +6,16 @@ import os
 
 file_id = "1NLj9MTHYH5z3N6d5YaXfMEG3qWmRyPNH"
 url = 'https://drive.google.com/file/d/1NLj9MTHYH5z3N6d5YaXfMEG3qWmRyPNH/view?usp=sharing'
+
 model_path = "trained_plant_disease_model.keras"
 
 
 if not os.path.exists(model_path):
     st.warning("Downloading model from Google Drive...")
-    gdown.download(url, model_path)
+    gdown.download(url, model_path, quiet=False)
 
+
+model_path = "trained_plant_disease_model.keras"
 def model_prediction(test_image):
     model = tf.keras.models.load_model(model_path)
     image = tf.keras.preprocessing.image.load_img(test_image,target_size=(128,128))
@@ -28,7 +31,7 @@ app_mode = st.sidebar.selectbox("Select Page",["HOME","DISEASE RECOGNITION"])
 
 # import Image from pillow to open images
 from PIL import Image
-img = Image.open("Diseases.jpeg")
+img = Image.open("Diseases.webp")
 
 # display image using streamlit
 # width is used to set the width of an image
@@ -50,5 +53,5 @@ elif(app_mode=="DISEASE RECOGNITION"):
         st.write("Our Prediction")
         result_index = model_prediction(test_image)
         #Reading Labels
-        class_name = ['Potato___Early_blight', 'Potato___Late_blight', 'Potato___healthy']
+        class_name = ['Potato__Early_blight', 'Potato_Late_blight', 'Potato__healthy']
         st.success("Model is Predicting it's a {}".format(class_name[result_index]))
